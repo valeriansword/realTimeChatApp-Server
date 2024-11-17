10579f1693154f3e7ef7a614a1de8f919f682513
valeriansword <chandrakumarxii@gmail.com>
2024-11-17 19:45:19 +0530

    added

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const ChatRouter=require("./routes/ChatRoute.js");
 dotenv.config()
 const {app,server}=require("./socket/socket.js")
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {origin:"*",
+        methods:["GET","POST","PUT","DELETE"]
+    }
+
+));
 
 const PORT=process.env.PORT || 3000;
 
